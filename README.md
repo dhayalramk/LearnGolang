4c609ba399d01fc67d3b8fb145dd25b08809df11
Dhayal Ram K <dhayalram.k@gmail.com>
2017-12-11 01:36:34 +0530

    Adding Project cards

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LearnGolang
 Learning Go Lang from basics
+
+
+Projects
+1) Hello world
+2) Cards
---
+++ cards/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds"
+	// card := newCard()
+
+	// fmt.Println(card)
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	// fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
